f6e86b720bfaa8ff6ed10f1475598d3fa6e320bd
Mateo Murphy <mateo.murphy@gmail.com>
2013-10-03 16:21:38 -0400

    Require login to post

--- client/client.js
+++ client/client.js
@@ -8,13 +8,11 @@ Template.messages.messages = function () {
 Template.input.events = {
   'keydown input#message' : function (event) {
     if (event.which == 13) { // 13 is the enter key event
-      if (Meteor.user())
-        var name = Meteor.user().profile.name;
-      else
-        var name = 'Anonymous';
       var message = document.getElementById('message');
 
-      if (message.value != '') {
+      if (Meteor.user() && message.value != '') {
+        var name = Meteor.user().profile.name;
+        
         Messages.insert({
           name: name,
           message: message.value,
